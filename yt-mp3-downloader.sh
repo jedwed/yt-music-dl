50e7301da96162b45f6c0a1e385413106e7b3705
jedwed <83894732+jedwed@users.noreply.github.com>
2026-03-31 19:19:04 +1100

    Initial commit: Added music json file and the downloader script

---
+++ yt-mp3-downloader.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+music_list_file=$1
+echo "Downloading music from $music_list_file"
+
+jq -c '.[]' "$music_list_file" | while read music_info; do
+	url=$(echo "$music_info" | jq -r '.url')
+	artist_first_name=$(echo "$music_info" | jq -r '.artist_first_name')
+	artist_last_name=$(echo "$music_info" | jq -r '.artist_last_name')
+	track_name=$(echo "$music_info" | jq -r '.track_name')
+	performer_first_name=$(echo "$music_info" | jq -r '.performer_first_name')
+	performer_last_name=$(echo "$music_info" | jq -r '.performer_last_name')
+	year=$(echo "$music_info" | jq -r '.year')
+
+	file_name="${artist_last_name} - ${track_name} (${performer_last_name})"
+	file_name_ext="${file_name}.m4a"
+	if [ -f "$file_name_ext" ]; then
+		echo "File ${file_name_ext} already exists"
+		continue
+	fi
+
+	yt-dlp -t aac "$url" -o "${file_name}.%(ext)s"
+	echo "Downloaded file ${file_name_ext}"
+
+	exiftool -Title="$track_name" -Artist="$artist_first_name $artist_last_name" -Year="$year" -Performer="$performer_first_name $performer_last_name" "$file_name_ext"
+done
